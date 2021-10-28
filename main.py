623d0873734dde1b53a37e3680b7258eac54b63b
Ric Arcifa <92729550+rarcif@users.noreply.github.com>
2021-10-28 12:46:35 +0000

    Added TVL function

--- main.py
+++ main.py
@@ -10,25 +10,33 @@ from keep_alive import keep_alive
 ###########
 # variables 
 ###########
-
-coin = "crypto-com-chain" # coin ID - coincecko
-currency = "usd" # currency conversion
-endpoint = f"https://api.coingecko.com/api/v3/simple/price?ids={coin}&vs_currencies={currency}" # endpoint - coingecko
-req = requests.get(url = endpoint) # request endpoint data
-data = req.json()[coin][currency] # convert data into json
+COIN = "crypto-com-chain" # coin ID - coincecko
+CURRENCY = "usd" # currency conversion
 BOT_TOKEN = os.environ['DISCORD_BOT_TOKEN']
+TVL = "total_value_locked"
+MARKET = "market_data"
 
-##############
-# notification 
-##############
+################
+# price endpoint 
+################
+PRICE_ENDPOINT = f"https://api.coingecko.com/api/v3/simple/price?ids={COIN}&vs_currencies={CURRENCY}" # endpoint - coingecko
+req = requests.get(url = PRICE_ENDPOINT) # request endpoint data
+price = req.json()[COIN][CURRENCY] # convert data into json
+
+###############
+# TLV endpoint 
+###############
+TLV_ENDPOINT = f"https://api.coingecko.com/api/v3/coins/{COIN}"
+req = requests.get(url = TLV_ENDPOINT) # request endpoint data
+tvl = req.json()[MARKET][TVL][CURRENCY]# convert data into json
+
+###########
+# functions
+###########
 
 # send discord notificaiton to a channel
 async def sendMessage(message):
   await discord.utils.get(client.get_all_channels(),name='general').send(message)
-
-##############
-# start client 
-##############
 
 # instantiate a discord client
 client = discord.Client()
@@ -48,11 +56,15 @@ async def on_message(message):
 
   # list commands
   if message.content.startswith('!help'):
-    await message.channel.send('Command List: \n  !help     prints the list of commands \n  !price     prints the current CRO price')
+    await message.channel.send('Command List: \n [!help] - prints the list of commands \n [!price] - prints the current CRO price \n [!tvl] - prints the total value locked'')
 
   # list current price 
   if message.content.lower() == "!price":
-    await message.channel.send(f'The current price of CRO is: {data}')
+    await message.channel.send(f'The current price of CRO is: {price} USD')
+
+  # list total value locked
+  if message.content.lower() == "!tvl":
+    await message.channel.send(f'The total value locked of CRO is: {tvl} USD')
 
 keep_alive() # keep server alive
 
