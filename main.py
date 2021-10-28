5a306c6ec2a9763e06fb9cb0eda38198d5332891
Ric Arcifa <92729550+rarcif@users.noreply.github.com>
2021-10-28 10:57:54 +0000

    cleaned documentation and syntax

--- main.py
+++ main.py
@@ -7,16 +7,28 @@ from keep_alive import keep_alive
     # !help   prints list of commands
     # !price  prints current price
 
-# variables
+###########
+# variables 
+###########
+
 coin = "crypto-com-chain" # coin ID - coincecko
 currency = "usd" # currency conversion
 endpoint = f"https://api.coingecko.com/api/v3/simple/price?ids={coin}&vs_currencies={currency}" # endpoint - coingecko
 req = requests.get(url = endpoint) # request endpoint data
 data = req.json()[coin][currency] # convert data into json
+BOT_TOKEN = os.environ['DISCORD_BOT_TOKEN']
+
+##############
+# notification 
+##############
 
 # send discord notificaiton to a channel
 async def sendMessage(message):
   await discord.utils.get(client.get_all_channels(),name='general').send(message)
+
+##############
+# start client 
+##############
 
 # instantiate a discord client
 client = discord.Client()
@@ -34,6 +46,7 @@ async def on_message(message):
   if message.author == client.user:
     return
 
+  # list commands
   if message.content.startswith('!help'):
     await message.channel.send('Command List: \n  !help     prints the list of commands \n  !price     prints the current CRO price')
 
@@ -43,5 +56,4 @@ async def on_message(message):
 
 keep_alive() # keep server alive
 
-BOT_TOKEN = os.environ['DISCORD_BOT_TOKEN']
 client.run(BOT_TOKEN)
